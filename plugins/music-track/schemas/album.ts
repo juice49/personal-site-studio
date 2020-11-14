d0f1799471177cbb393f1d61cab69ae3e9a9e505
Ash <ashley@juice49.com>
2020-11-14 17:58:57 +0000

    Add missing `images` field to `album` schema

--- plugins/music-track/schemas/album.ts
+++ plugins/music-track/schemas/album.ts
@@ -19,5 +19,30 @@ export default {
       type: 'image',
       validation: rule => rule.required().error('Album image not processed.'),
     },
+    {
+      title: 'Images',
+      name: 'images',
+      type: 'array',
+      of: [
+        {
+          title: 'Image',
+          name: 'image',
+          type: 'object',
+          fields: [
+            {
+              title: 'Dimensions',
+              name: 'dimensions',
+              type: 'array',
+              of: [{ type: 'number' }],
+            },
+            {
+              title: 'URL',
+              name: 'url',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
   ],
 }
