e0c6ee85fed1c69955f6d1b334c23770ba4df5b5
Ash <ashley@juice49.com>
2022-12-16 20:08:01 +0000

    Update Sanity Studio

--- plugins/music-track/index.ts
+++ plugins/music-track/index.ts
@@ -1,11 +1,11 @@
-import { createPlugin } from 'sanity'
+import { definePlugin } from 'sanity'
 import track from './schemas/track'
 import artist from './schemas/artist'
 import album from './schemas/album'
 import platformData from './schemas/platform-data'
 import platformTrackData from './schemas/platform-track-data'
 
-export default createPlugin(() => {
+export default definePlugin(() => {
   return {
     name: 'music-track',
     schema: {
