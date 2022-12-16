ac37b0e8b2733814ba41278e73f25122a5a44dc9
Ash <ashley@juice49.com>
2022-12-16 20:08:01 +0000

    Update Sanity Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,11 @@
-import { createConfig } from 'sanity'
+import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import jam from './schemas/jam'
 import musicTrack from './plugins/music-track'
 import { defaultDocumentNode } from './plugins/music-track/track-structure'
 
-export default createConfig({
+export default defineConfig({
   name: 'ash',
   projectId: 'pbj0j3k8',
   dataset: 'production',
